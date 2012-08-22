518cad95e8601a108b9278772030e7bf3f49f9b0
Shinichiro Suzuki <shinichiro.su@gmail.com>
2012-08-22 10:08:56 +0900

    add doctest

--- Scraper.hs
+++ Scraper.hs
@@ -45,23 +45,14 @@ hrefs ((TagOpen "a" [("href", url)]) : ts) = url : hrefs ts
 hrefs ((TagOpen "a" [("href", url), _]) : ts) = url : hrefs ts
 hrefs (_:ts) = hrefs ts
 
--- testHtmlStr = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" /><link href=\"css/bootstrap-responsive.css\" rel=\"stylesheet\" /></head><body><div id=\"container\"><div id=\"content\"><div class=\"alert alert-success\" id=\"notification\" style=\"display:none\"> ... </div><h1><a href=\"/\">GYMMEMO</a></h1><table class=\"table\"><tr><th>日時</th><th>名前</th><th>スコア</th></tr><tr><td>hoho</td><td class=\"date\">2012-09-09</td><td>ooo</td></tr></table></div></div><script type=\"text/javascript\" src=\"js/jquery.js\"></script><script type=\"text/javascript\" src=\"js/bootstrap-tab.js\"></script><script type=\"text/javascript\" src=\"js/bootstrap-button.js\"></script><script type=\"text/javascript\" src=\"js/websql.js\"></script></body></html>"
--- *Scraper> let html = tagTree $ parseTags testHtmlStr 
--- *Scraper> let tr = subTree (Just "tr") Nothing html
--- *Scraper> tr
+-- | subTree
+-- 
+-- >>> subTree (Just "tr") Nothing $ tagTree $ parseTags "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" /><link href=\"css/bootstrap-responsive.css\" rel=\"stylesheet\" /></head><body><div id=\"container\"><div id=\"content\"><div class=\"alert alert-success\" id=\"notification\" style=\"display:none\"> ... </div><h1><a href=\"/\">GYMMEMO</a></h1><table class=\"table\"><tr><th>日時</th><th>名前</th><th>スコア</th></tr><tr><td>hoho</td><td class=\"date\">2012-09-09</td><td>ooo</td></tr></table></div></div><script type=\"text/javascript\" src=\"js/jquery.js\"></script><script type=\"text/javascript\" src=\"js/bootstrap-tab.js\"></script><script type=\"text/javascript\" src=\"js/bootstrap-button.js\"></script><script type=\"text/javascript\" src=\"js/websql.js\"></script></body></html>"
 -- [TagBranch "tr" [] [TagBranch "th" [] [TagLeaf (TagText "\26085\26178")],TagBranch "th" [] [TagLeaf (TagText "\21517\21069")],TagBranch "th" [] [TagLeaf (TagText "\12473\12467\12450")]],TagBranch "tr" [] [TagBranch "td" [] [TagLeaf (TagText "hoho")],TagBranch "td" [("class","date")] [TagLeaf (TagText "2012-09-09")],TagBranch "td" [] [TagLeaf (TagText "ooo")]]]
--- *Scraper> length tr
--- 2
--- *Scraper> let td = subTree (Just "td") Nothing html
--- *Scraper> td
+-- 
+-- >>> subTree (Just "td") Nothing $ tagTree $ parseTags "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" /><link href=\"css/bootstrap-responsive.css\" rel=\"stylesheet\" /></head><body><div id=\"container\"><div id=\"content\"><div class=\"alert alert-success\" id=\"notification\" style=\"display:none\"> ... </div><h1><a href=\"/\">GYMMEMO</a></h1><table class=\"table\"><tr><th>日時</th><th>名前</th><th>スコア</th></tr><tr><td>hoho</td><td class=\"date\">2012-09-09</td><td>ooo</td></tr></table></div></div><script type=\"text/javascript\" src=\"js/jquery.js\"></script><script type=\"text/javascript\" src=\"js/bootstrap-tab.js\"></script><script type=\"text/javascript\" src=\"js/bootstrap-button.js\"></script><script type=\"text/javascript\" src=\"js/websql.js\"></script></body></html>"
 -- [TagBranch "td" [] [TagLeaf (TagText "hoho")],TagBranch "td" [("class","date")] [TagLeaf (TagText "2012-09-09")],TagBranch "td" [] [TagLeaf (TagText "ooo")]]
--- *Scraper> length td
--- 3
--- *Scraper> mapM_ print td
--- TagBranch "td" [] [TagLeaf (TagText "hoho")]
--- TagBranch "td" [("class","date")] [TagLeaf (TagText "2012-09-09")]
--- TagBranch "td" [] [TagLeaf (TagText "ooo")]
--- *Scraper>
+
 subTree :: Eq a => Maybe a -> Maybe [Attribute a] -> [TagTree a] -> [TagTree a]
 subTree _ _ [] = []
 subTree (Just tagString) Nothing trees = subTreeByTag tagString trees
